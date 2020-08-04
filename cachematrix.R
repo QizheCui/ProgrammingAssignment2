a12b5b7f0adf71d61b776db189996ce4acd3c208
崔启哲 <cuiqizhe@cuiqizhedeMacBook-Pro.local>
2020-08-04 15:26:39 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL      #we want the default value of inverse to be NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL      #Once we set another matrix it can override the orginal matrix
+  }
+  get<-function(){
+    x
+  }
+  setinverse<-function(inverse){
+    m<<-inverse
+  }
+  getinverse<-function(){
+    m
+  }
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if (! is.null(m)){ #check if the matrix int the getINv() is not NULL
+    print("getting cached matrix inversion")
+    m
+  }
+  data<-x$get()
+  m<-solve(data)
+  x$setinverse(m,...)#cache the inverse matrix 
+  m 
+  ## Return a matrix that is the inverse of 'x'
 }
